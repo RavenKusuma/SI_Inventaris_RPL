419d051ed777b37b632dbb258eee065dab471bdb
RavenRindou <rtejakusuma21@yahoo.com>
2017-11-09 14:53:49 +0700

    system login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,23 +3,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+  public function index()
+  {
+    $this->load->view('v_header');
+    $this->load->view('home/v_login');
+    $this->load->view('v_footer');
+  }
+
+  public function logindata()
 	{
-		$this->load->view('welcome_message');
+		$data = array('username' => $this->input->post('username'),
+									'password' => $this->input->post('password'),
+	 								);
+    $cek = $this->login_m->check_data($data);
+
+    if ($cek->num_rows() == 0) {
+        $this->session->set_flashdata('message', 'data tidak ditemukan, silahkan coba lagi');
+        redirect('Login');
+    }
+
+    else {
+
+        $cek2 = $cek->result_array();
+        $userdata = array('username' => $cek2[0]['username'],
+                          'password' => $cek2[0]['password'],
+                          'rule'     => "user"
+                         );
+        $this->session->set_userdata($userdata);
+        redirect('User');
+    }
 	}
 }
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-inverse navbar-fixed-bottom text-center">
+  <p style="padding-top:1%; color:white">&copy; Software Engineering</p>
+  <ul class="nav navbar-nav navbar-right">
+  </ul>
+  </div>
+</nav>
