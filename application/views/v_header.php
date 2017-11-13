bd75125877ffd6c340966f27fce0c43a0c35ff5d
RavenRindou <rtejakusuma21@yahoo.com>
2017-11-13 15:16:27 +0700

    sudah bisa nyambung ke login

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -6,13 +6,13 @@
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
   <!-- Latest compiled and minified JavaScript -->
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-    <title>Tutorial CodeIgniter</title>
+    <title>SI Inventaris</title>
   </head>
 
   <nav class="navbar navbar-inverse">
     <a class="navbar-brand" style="color:white" href="#">Software Engineering</a>
     <ul class="nav navbar-nav navbar-right" style="padding-right:15px">
-        <li><a href="<?php echo base_url();?>Login/index"><span class="glyphicon glyphicon-user"></span> Login</a></li>
-        <li><a href="<?php echo base_url();?>Register/index"><span class="glyphicon glyphicon-list"></span> Register</a></li>
+        <li><a href="<?php echo base_url();?>login_message/index"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+        <li><a href="<?php echo base_url();?>register_message/index"><span class="glyphicon glyphicon-list"></span> Register</a></li>
     </ul>
   </nav>
